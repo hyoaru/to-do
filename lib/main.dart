ddd59001e79d40d59798ba282d6e5ed537e41eea
hyoaru <jjcabreraaaa@gmail.com>
2021-08-16 19:38:07 +0800

    Added initial ui setup

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sizer/sizer.dart';
+import 'package:todo/view/homepage_ui.dart';
+
+void main() {
+  runApp(const ProviderScope(child: TodoListApp()));
+}
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'To-Do List App',
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              textTheme: GoogleFonts.latoTextTheme(
+                Theme.of(context).textTheme,
+              )),
+          home: const HomepageUI(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/view/homepage_ui.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:sizer/sizer.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+class HomepageUI extends HookWidget {
+  const HomepageUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final todoTextController = useTextEditingController();
+
+    return Scaffold(
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 800),
+          child: ListView(
+            padding: EdgeInsets.only(left: 10.w, right: 10.w, top: 5.h),
+            children: [
+              const HomeTitle(),
+              TodoTextField(todoTextController: todoTextController),
+              SizedBox(
+                height: 10.h,
+              ),
+              Column(
+                children: [],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeTitle extends StatelessWidget {
+  const HomeTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'todos',
+          style: TextStyle(fontSize: 30.sp),
+        ),
+        Icon(
+          Icons.check,
+          size: 30.sp,
+        ),
+      ],
+    );
+  }
+}
+
+class TodoTextField extends StatelessWidget {
+  const TodoTextField({
+    Key? key,
+    required this.todoTextController,
+  }) : super(key: key);
+
+  final TextEditingController todoTextController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: todoTextController,
+      decoration: InputDecoration(
+        labelText: 'What needs to be done?',
+        contentPadding: EdgeInsets.only(top: 0.5.h, bottom: 0.5.h),
+
+        // labelStyle: Textsty
+      ),
+      onSubmitted: (value) {
+        //
+      },
+    );
+  }
+}
