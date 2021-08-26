6c1da3ad9ef5d350d92021526298e904e40b757a
hyoaru <jjcabreraaaa@gmail.com>
2021-08-26 18:21:09 +0800

    removed list from the main ui file

--- lib/view/todo_homepage_ui.dart
+++ lib/view/todo_homepage_ui.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:sizer/sizer.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo/model/todo_model.dart';
+import 'package:todo/view/todo_homepage_list_item.dart';
 import 'package:todo/view/todo_homepage_textfield.dart';
 import 'package:todo/view/todo_homepage_title.dart';
 import 'package:todo/viewmodel/todo_providers.dart';
@@ -46,58 +47,6 @@ class HomepageUI extends HookWidget {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class TodoItem extends HookWidget {
-  const TodoItem(this.todo, {Key? key}) : super(key: key);
-
-  final TodoObjectModel todo;
-
-  @override
-  Widget build(BuildContext context) {
-    final itemFocusNode = useFocusNode();
-    useListenable(itemFocusNode);
-    final isFocused = itemFocusNode.hasFocus;
-
-    final textEdittingController = useTextEditingController();
-    final textFieldFocusNode = useFocusNode();
-
-    return Material(
-      // color: Colors.white,
-      // elevation: 6,
-      child: Focus(
-        focusNode: itemFocusNode,
-        onFocusChange: (focused) {
-          if (focused) {
-            textEdittingController.text = todo.description;
-          } else {
-            context.read(todoListProvider.notifier).todoEdit(
-                  id: todo.id,
-                  description: textEdittingController.text,
-                );
-          }
-        },
-        child: ListTile(
-          onTap: () {
-            itemFocusNode.requestFocus();
-            textFieldFocusNode.requestFocus();
-          },
-          leading: Checkbox(
-            value: todo.isCompleted,
-            onChanged: (value) =>
-                context.read(todoListProvider.notifier).todoToggle(todo.id),
-          ),
-          title: isFocused
-              ? TextField(
-                  autofocus: true,
-                  focusNode: textFieldFocusNode,
-                  controller: textEdittingController,
-                )
-              : Text(todo.description),
         ),
       ),
     );
