18cb6c766d3725822aa695b543fdb71b659937c9
hyoaru <jjcabreraaaa@gmail.com>
2021-08-26 18:20:06 +0800

    separated list from main ui file

---
+++ lib/view/todo_homepage_list_item.dart
@@ -0,0 +1,64 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:todo/view/todo_homepage_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:todo/model/todo_model.dart';
+import 'package:todo/viewmodel/todo_providers.dart';
+import 'package:todo/model/todo_notifiers.dart';
+
+late final ProviderReference ref;
+
+class TodoItem extends HookWidget {
+  const TodoItem(this.todo, {Key? key}) : super(key: key);
+
+  final TodoObjectModel todo;
+
+  @override
+  Widget build(BuildContext context) {
+    final itemFocusNode = useFocusNode();
+    useListenable(itemFocusNode);
+    final isFocused = itemFocusNode.hasFocus;
+
+    final textEdittingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Material(
+      // color: Colors.white,
+      // elevation: 6,
+      child: Focus(
+        focusNode: itemFocusNode,
+        onFocusChange: (focused) {
+          if (focused) {
+            textEdittingController.text = todo.description;
+          } else {
+            final value = ref.read(todoListProvider.notifier).todoEdit(
+                  id: todo.id,
+                  description: textEdittingController.text,
+                );
+            return value;
+          }
+        },
+        child: ListTile(
+          onTap: () {
+            itemFocusNode.requestFocus();
+            textFieldFocusNode.requestFocus();
+          },
+          leading: Checkbox(
+              value: todo.isCompleted,
+              onChanged: (value) {
+                final value =
+                    context.read(todoListProvider.notifier).todoToggle(todo.id);
+                return value;
+              }),
+          title: isFocused
+              ? TextField(
+                  autofocus: true,
+                  focusNode: textFieldFocusNode,
+                  controller: textEdittingController,
+                )
+              : Text(todo.description),
+        ),
+      ),
+    );
+  }
+}
