7be8d7ae726c99ff8105bdf03de580025cf966ae
hyoaru <jjcabreraaaa@gmail.com>
2021-08-27 13:37:41 +0800

    added toolbar class to main ui

--- lib/view/todo_homepage_ui.dart
+++ lib/view/todo_homepage_ui.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo/view/todo_homepage_list_item.dart';
 import 'package:todo/view/todo_homepage_textfield.dart';
 import 'package:todo/view/todo_homepage_title.dart';
+import 'package:todo/view/todo_homepage_toolbar.dart';
 import 'package:todo/viewmodel/todo_providers.dart';
 
 class HomepageUI extends HookConsumerWidget {
@@ -15,36 +16,40 @@ class HomepageUI extends HookConsumerWidget {
     final todos = ref.watch(filteredTodos);
     final todoTextController = useTextEditingController();
 
-    return Scaffold(
-      body: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 800),
-          child: ListView(
-            padding: EdgeInsets.only(left: 10.w, right: 10.w, top: 5.h),
-            children: [
-              const HomeTitle(),
-              TodoTextField(todoTextController: todoTextController),
-              SizedBox(
-                height: 3.h,
-              ),
-              Column(
-                children: [
-                  // if (todos.isNotEmpty) const Divider(height: 0),
-                  for (var i = 0; i < todos.length; i++) ...[
-                    if (i > 0) const Divider(height: 0),
-                    Dismissible(
-                      key: ValueKey(todos[i].id),
-                      onDismissed: (_) {
-                        ref
-                            .read(todoListProvider.notifier)
-                            .todoRemove(todos[i]);
-                      },
-                      child: TodoItem(todos[i]),
-                    ),
-                  ]
-                ],
-              ),
-            ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 800),
+            child: ListView(
+              padding: EdgeInsets.only(left: 10.w, right: 10.w, top: 5.h),
+              children: [
+                const HomeTitle(),
+                TodoTextField(todoTextController: todoTextController),
+                // SizedBox(
+                //   height: 3.h,
+                // ),
+                const Toolbar(),
+                Column(
+                  children: [
+                    // if (todos.isNotEmpty) const Divider(height: 0),
+                    for (var i = 0; i < todos.length; i++) ...[
+                      if (i > 0) const Divider(height: 0),
+                      Dismissible(
+                        key: ValueKey(todos[i].id),
+                        onDismissed: (_) {
+                          ref
+                              .read(todoListProvider.notifier)
+                              .todoRemove(todos[i]);
+                        },
+                        child: TodoItem(todos[i]),
+                      ),
+                    ]
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
